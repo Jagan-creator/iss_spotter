6b83626655afb61faa84441d8ec6a6bbadb87849
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-17 15:24:56 -0700

    refactored our code from iss.js and inde.js to include the use of promises which avoide the problem of callback hell from happening

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);             //date here
+    datetime.setUTCSeconds(pass.risetime);    //rise time
+    const duration = pass.duration;           //for how long
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
