92289de46bac6a23e2485e26507f3c8c36ddd4e1
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-17 12:13:42 -0700

    linting

--- iss.js
+++ iss.js
@@ -34,7 +34,7 @@ const fetchCoordsByIP = function(ip, callback) {
       const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
       callback(Error(message), null);
       return;
-    } 
+    }
 
     const { latitude, longitude } = parsedBody;
     callback(null, {latitude, longitude});    //callback to return the lat and long object
