416d8a912ba48b94800ef68cf75bdc33e7dfe430
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-16 20:19:51 -0700

    added in a function that checks for lat and long coordinates and then output the console via our index.js file

--- iss.js
+++ iss.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+///////////////////////////
+
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     
@@ -16,4 +18,32 @@ const fetchMyIP = function(callback) {
   });
 };
 
+///////////////////////////
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {    //catching any inital errors
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {    //testing to see if the JSON.parse was successful
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const { latitude, longitude } = parsedBody;
+    callback(null, {latitude, longitude});    //callback to return the lat and long object
+
+  });
+};
+
+///////////////////////////
+
+
 module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
