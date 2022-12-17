6328187493d1487bd1674d206353ad0602be33a5
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-17 12:12:34 -0700

    added in fetching function for passover coordinates and also added in an output function in the index to say whether the results worked or not

--- iss.js
+++ iss.js
@@ -44,6 +44,26 @@ const fetchCoordsByIP = function(ip, callback) {
 
 ///////////////////////////
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {    //catch any initial errors
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {    //making sure response from destination url is successful or not
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);   //callback to return our pass times from the url
+
+  });
+};
 
 module.exports = { fetchMyIP };
 module.exports = { fetchCoordsByIP };
+module.exports = { fetchISSFlyOverTimes };
